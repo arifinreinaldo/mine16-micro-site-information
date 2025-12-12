3bf487b2ff9e01536087fea2bdfbb9bce2aadf46
Reinaldo-GK <arifin.reinaldo@gmail.com>
2025-12-12 11:35:36 +0800

    bug fix favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,7 @@ export const metadata: Metadata = {
   description: "Your trusted pet profile and contact resource.",
   keywords: ["pet", "pet profile", "contact owner"],
   icons: {
-    icon: [
-      {
-        url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🐾</text></svg>",
-        type: "image/svg+xml",
-      },
-    ],
+    icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='.9em' font-size='90'%3E🐾%3C/text%3E%3C/svg%3E",
   },
 };
 
